ec9c70b2bdff0397830c1a3160b725e98b2a2b6c
yeonee333 <hsh12237@hanyang.ac.kr>
2025-12-08 16:31:22 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,83 @@
+// data.js
+const restaurantList = [
+    {
+        name: "홍성 마라미방",
+        category: "중식",
+        address: "경기도 안산시 상록구 사동 1149-21",
+        desc: "내 맘대로 골라 담는 마라탕과 바삭한 꿔바로우",
+        menu: "마라탕, 꿔바로우",
+        img: "images/mara.jpg"
+    },
+    {
+        name: "인더비엣",
+        category: "베트남식",
+        address: "경기도 안산시 상록구 성안길 78-3",
+        desc: "진한 고기 육수의 쌀국수, 에리카 핫플레이스",
+        menu: "쌀국수, 월남쌈",
+        img: "images/viet.jpg"
+    },
+    {
+        name: "에비수",
+        category: "일식",
+        address: "경기도 안산시 상록구 사동 1568-6",
+        desc: "깔끔하고 정갈한 일본 가정식 덮밥",
+        menu: "덮밥, 우동",
+        img: "images/ebisu.jpg"
+    },
+    {
+        name: "동양솥밥",
+        category: "한식",
+        address: "경기도 안산시 상록구 사동 1569-2",
+        desc: "갓 지은 따끈한 솥밥 한 그릇의 여유",
+        menu: "스테이크 솥밥, 전복 솥밥",
+        img: "images/rice.jpg"
+    },
+    {
+        name: "파익스피크소다",
+        category: "카페",
+        address: "경기도 안산시 상록구 한양대학로 48",
+        desc: "힙한 분위기에서 즐기는 다양한 소다 음료",
+        menu: "소다 음료, 커피",
+        img: "images/soda.jpg"
+    },
+    {
+        name: "레이지그라운드",
+        category: "카페",
+        address: "경기도 안산시 상록구 사동 1149",
+        desc: "공강 시간에 쉬어가기 좋은 감성 카페",
+        menu: "아메리카노, 크로플",
+        img: "images/lazy.jpg"
+    },
+    {
+        name: "찌개찌개",
+        category: "한식",
+        address: "경기도 안산시 상록구 사동 1569-4",
+        desc: "어머니 손맛이 느껴지는 가성비 찌개 맛집",
+        menu: "김치찌개, 된장찌개",
+        img: "images/stew.jpg"
+    },
+    {
+        name: "카바레식당",
+        category: "일식",
+        address: "경기도 안산시 상록구 한양대학로 66",
+        desc: "깊은 풍미의 교토식 카레 전문점",
+        menu: "카레, 가라아게",
+        img: "images/curry.jpg"
+    },
+    {
+        name: "북경반점",
+        category: "중식",
+        address: "경기도 안산시 상록구 학사4길 1",
+        desc: "불향 가득한 쟁반짜장이 일품인 중국집",
+        menu: "짜장면, 짬뽕",
+        img: "images/china.jpg"
+    },
+    {
+        name: "현가네 병천순대",
+        category: "한식",
+        address: "경기도 안산시 상록구 사동 1561-2",
+        desc: "속이 꽉 찬 순대와 뜨끈한 국밥",
+        menu: "순대국밥, 모듬순대",
+        img: "images/sundae.jpg"
+    }
+];
